6a2208314da4ef09bb980d13c2b65c9357744c34
Nikita-Babich <nikita.babich.a@gmail.com>
2024-01-19 02:29:00 +0300

    + images + opening + QImage <-> QvecQvecQColor converters

--- rework/rework.cpp
+++ rework/rework.cpp
@@ -1,10 +1,33 @@
 #include "rework.h"
 
+// This file contains application procedures.
+
 rework::rework(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+    //fill combobox here with new filter
+
 }
 
 rework::~rework()
 {}
+
+void rework::on_openButton_clicked() {
+	try{
+        QString fileName = QFileDialog::getOpenFileName(this, tr("Open Image"), "", tr("Images (*.png *.xpm *.jpg *.ppm)"));
+        if (!fileName.isEmpty()) {
+            QImage image(fileName);
+            if (!image.isNull()) {
+                ui.imageLabel1->setPixmap(QPixmap::fromImage(image));
+            }
+            else {
+
+            }
+        }
+	}
+	catch (const std::exception&)
+	{
+
+	}
+}
--- rework/rework.h
+++ rework/rework.h
@@ -1,7 +1,20 @@
 #pragma once
 
+// This file contains
+// 1. class rework and procedures' definitions
+// 2. compatibility functions
+// 3. filters
+
 #include <QtWidgets/QMainWindow>
 #include "ui_rework.h"
+
+#include <QImage>
+//https://doc.qt.io/qt-6/qimage.html#reading-and-writing-image-files
+
+#include <QFileDialog>
+
+#include <QVector>
+#include <QColor>
 
 class rework : public QMainWindow
 {
@@ -13,4 +26,33 @@ public:
 
 private:
     Ui::reworkClass ui;
+    QImage start_image, new_image, new_image2;
+
+private slots:
+    void on_openButton_clicked();
 };
+
+//Comptibility with older functions?
+QVector<QVector<QColor>> imageToVector(const QImage& image) {
+    QVector<QVector<QColor>> result;
+    for (int y = 0; y < image.height(); ++y) {
+        QVector<QColor> row;
+        for (int x = 0; x < image.width(); ++x) {
+            row.append(image.pixelColor(x, y));
+        }
+        result.append(row);
+    }
+    return result;
+}
+
+QImage vectorToImage(const QVector<QVector<QColor>>& colorVector) {
+    int width = colorVector.isEmpty() ? 0 : colorVector[0].size();
+    int height = colorVector.size();
+    QImage result(width, height, QImage::Format_ARGB32); // not sure about the format
+    for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+            result.setPixelColor(x, y, colorVector[y][x]);
+        }
+    }
+    return result;
+}
